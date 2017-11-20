943526cddd8d97a486b3bfb74496b2c52285502d
zhengpeng7 <15732115701@163.com>
2017-11-20 22:09:02 +0000

    Upgrade CapsNet

--- FashionMNIST_Challenge/CapsNet-Fashion-MNIST/capsulenet.py
+++ FashionMNIST_Challenge/CapsNet-Fashion-MNIST/capsulenet.py
@@ -154,7 +154,7 @@ if __name__ == "__main__":
     # setting the hyper parameters
     import argparse
     parser = argparse.ArgumentParser()
-    parser.add_argument('--batch_size', default=60, type=int)
+    parser.add_argument('--batch_size', default=30, type=int)
     parser.add_argument('--epochs', default=10, type=int)
     parser.add_argument('--lam_recon', default=0.392, type=float)  # 784 * 0.0005, paper uses sum of SE, here uses MSE
     parser.add_argument('--num_routing', default=3, type=int)  # num_routing should > 0
